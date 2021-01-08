7134521d9da9001579fec4c01877606898e0017c
bkj <ben@canfield.io>
2021-01-08 20:20:29 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# build.sh
+
+# --
+# Build
+
+rm -rf build
+mkdir build
+cd build
+cmake ..
+make -j12
+cd ..
---
+++ sssp.cu
@@ -0,0 +1,71 @@
+#include <cstdlib>  // EXIT_SUCCESS
+#include "stdio.h"
+
+#include <gunrock/applications/sssp.hxx>
+
+using namespace gunrock;
+using namespace memory;
+
+void test_sssp(int num_arguments, char** argument_array) {
+  if (num_arguments != 2) {
+    std::cerr << "usage: ./bin/<program-name> filename.mtx" << std::endl;
+    exit(1);
+  }
+
+  // --
+  // Define types
+
+  using vertex_t = int;
+  using edge_t = int;
+  using weight_t = float;
+
+  // --
+  // IO
+
+  std::string filename = argument_array[1];
+
+  io::matrix_market_t<vertex_t, edge_t, weight_t> mm;
+  format::csr_t<memory::memory_space_t::device, vertex_t, edge_t, weight_t> csr;
+  csr.from_coo(mm.load(filename));
+
+  // --
+  // Build graph
+
+  auto G = graph::build::from_csr<memory_space_t::device, graph::view_t::csr>(
+      csr.number_of_rows,               // rows
+      csr.number_of_columns,            // columns
+      csr.number_of_nonzeros,           // nonzeros
+      csr.row_offsets.data().get(),     // row_offsets
+      csr.column_indices.data().get(),  // column_indices
+      csr.nonzero_values.data().get()   // values
+  );  // supports row_indices and column_offsets (default = nullptr)
+
+  // --
+  // Params and memory allocation
+
+  vertex_t single_source = 1;
+
+  vertex_t n_vertices = G.get_number_of_vertices();
+  thrust::device_vector<weight_t> distances(n_vertices);
+  thrust::device_vector<vertex_t> predecessors(n_vertices);
+
+  // --
+  // Run problem
+
+  float elapsed = gunrock::sssp::run(G, single_source, distances.data().get(),
+                                     predecessors.data().get());
+
+  // --
+  // Log
+
+  std::cout << "Distances (output) = ";
+  thrust::copy(distances.begin(), distances.end(),
+               std::ostream_iterator<weight_t>(std::cout, " "));
+  std::cout << std::endl;
+  std::cout << "SSSP Elapsed Time: " << elapsed << " (ms)" << std::endl;
+}
+
+int main(int argc, char** argv) {
+  test_sssp(argc, argv);
+  return EXIT_SUCCESS;
+}
