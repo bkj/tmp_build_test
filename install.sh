d5341e7ef1178e86671996adf5eff06f33cef680
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2021-03-18 14:09:42 +0000

    ...

--- install.sh
+++ install.sh
@@ -2,4 +2,23 @@
 
 # install.sh
 
+# --
+# Setup python environment
+
+conda create -y -n pygunrock_env python=3.7
+conda activate pygunrock_env
+conda install -y -c pytorch pytorch==1.7.1 cudatoolkit=11.0
+
+pip install scipy
+
+# --
+# Clone pybind11
+
 git clone https://github.com/pybind/pybind11
+
+# --
+# Build
+
+export ANACONDA_PATH=/home/ubuntu/anaconda3
+export PYTORCH_VERSION=pytorch-1.7.1-py3.7_cuda11.0.221_cudnn8.0.5_0 # ls $ANACONDA_PATH/pkgs | fgrep pytorch-1.7.1
+./build.sh
