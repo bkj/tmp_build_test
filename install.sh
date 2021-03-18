5b1cdbae220477dfadd5b308ac70a04c80b5ff39
Ubuntu <ubuntu@ip-172-31-84-141.ec2.internal>
2021-03-18 21:25:37 +0000

    install

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# run.sh
+
+# --
+# Install anaconda
+
+wget https://repo.anaconda.com/archive/Anaconda3-2020.11-Linux-x86_64.sh
+bash Anaconda3-2020.11-Linux-x86_64.sh
+rm Anaconda3-2020.11-Linux-x86_64.sh
+
+# --
+# Setup environment
+
+conda create -y -n pyg_env python=3.7
+conda activate pyg_env
+
+# dependencies
+conda install -y -c pytorch -c conda-forge \
+    pytorch=1.8.0 cudatoolkit=11.1 
+
+pip install scipy
+pip install tqdm
+
+# --
+# pybind11
+
+git clone https://github.com/pybind/pybind11
+
+# --
+# build
+
+./build.sh # > build.log
+
+# --
+# test
+
+python test.py
