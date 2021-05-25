5799037b50086cd594d4944c132d913176d3b358
Ubuntu <ubuntu@ip-172-31-92-82.ec2.internal>
2021-05-25 04:05:50 +0000

    ...

--- install.sh
+++ install.sh
@@ -19,6 +19,6 @@ git clone https://github.com/pybind/pybind11
 # --
 # Build
 
-export ANACONDA_PATH=/home/ubuntu/anaconda3
+export ANACONDA_PATH=/home/ubuntu/anaconda3/envs/pygunrock_env/
 export PYTORCH_VERSION=pytorch-1.7.1-py3.7_cuda11.0.221_cudnn8.0.5_0 # ls $ANACONDA_PATH/pkgs | fgrep pytorch-1.7.1
 ./build.sh
